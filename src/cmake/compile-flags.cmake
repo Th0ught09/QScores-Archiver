e24271702f9023b7d8b604bd12f218ced79272cf
Raymond Wan <rwan.work@gmail.com>
2025-03-01 18:03:35 +0000

    Changed the MY_CXX_FLAGS to release, removing the warning from variables that are set but never used.

--- src/cmake/compile-flags.cmake
+++ src/cmake/compile-flags.cmake
@@ -21,13 +21,13 @@
 
 
 ##  Testing compilation flags, some of which are suggested by the Valgrind 3.3 book
-set (MY_CXX_FLAGS "-pedantic -Wno-long-long -g -fno-inline -O0 -Wall")
+##  set (MY_CXX_FLAGS "-pedantic -Wno-long-long -g -fno-inline -O0 -Wall")
 
 ##  Release compilation flags, suggested by the Valgrind 3.3 book
 ##  Added the following because of warnings in Boost 1.59.0:
 ##    -Wno-unused-variable
 ##    -Wno-unused-but-set-variable
-# set (MY_CXX_FLAGS "-O3 -Wall -Wno-unused-variable -Wno-unused-but-set-variable")
+set (MY_CXX_FLAGS "-O3 -Wall -Wno-unused-variable -Wno-unused-but-set-variable")
 
 ##  Set compiler flags based on global variable (if there are any)
 set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${MY_CXX_FLAGS}")
